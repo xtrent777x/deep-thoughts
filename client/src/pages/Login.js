94400a1b2d5b039e5e22871e8e2e80593b221fc7
Trent Roesbery <xtrent777x@gmail.com>
2021-10-28 19:06:38 -0600

    bug fix

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,42 +1,41 @@
 import React, { useState } from 'react';
-
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
-const Login = props => {
+const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
-  const handleChange = event => {
+  const handleChange = (event) => {
     const { name, value } = event.target;
 
     setFormState({
       ...formState,
-      [name]: value
+      [name]: value,
     });
   };
 
   // submit form
-  const handleFormSubmit = async event => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-  
+
     try {
       const { data } = await login({
-        variables: { ...formState }
+        variables: { ...formState },
       });
-    
+
       Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
     }
-  };
 
     // clear form values
     setFormState({
       email: '',
-      password: ''
+      password: '',
     });
   };
 
@@ -69,12 +68,13 @@ const Login = props => {
                 Submit
               </button>
             </form>
+
             {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
     </main>
   );
-
+};
 
 export default Login;
